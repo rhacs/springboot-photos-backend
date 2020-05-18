62a03223334cb8ab12c9977c2a9d683f46695ca4
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-05-17 22:25:21 -0400

    Initial Commit

---
+++ src/main/java/cl/rhacs/springboot/photos/PhotosApplication.java
@@ -0,0 +1,13 @@
+package cl.rhacs.springboot.photos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/rhacs/springboot/photos/PhotosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.rhacs.springboot.photos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
