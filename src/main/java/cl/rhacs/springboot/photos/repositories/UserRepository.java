1c1198b279609ab1ff5c7db42bfc9b40de200663
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-05-19 22:35:37 -0400

    Updated UserRepository

--- src/main/java/cl/rhacs/springboot/photos/repositories/UserRepository.java
+++ src/main/java/cl/rhacs/springboot/photos/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package cl.rhacs.springboot.photos.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,21 @@ import cl.rhacs.springboot.photos.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * Searches the repository for the specified email address
+     *
+     * @param email the email address to find
+     * @return the user details if exists
+     */
+    public Optional<User> findByEmail(String email);
+
+    /**
+     * Searches the repository for the specified username
+     *
+     * @param username the username to find
+     * @return the user details if exists
+     */
+    public Optional<User> findByUsername(String username);
 
 }
